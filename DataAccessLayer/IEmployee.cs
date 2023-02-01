8aa719c5636899f389fd72e018d058cf365a2ee2
wafaeaissaoui <wafaeissa17@gmail.com>
2023-02-01 09:45:01 +0100

    first commit  project Blazor

---
+++ DataAccessLayer/IEmployee.cs
@@ -0,0 +1,16 @@
+﻿using BlazorApp2.Models;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BlazorApp2.DataAccessLayer
+{
+    public interface IEmployee
+    {
+          void create( employe e);
+          void update(employe e);
+          void  delete(int id );
+          IEnumerable employes();
+          employe get(int id);
+    }
+}
---
+++ Models/employe.cs
@@ -0,0 +1,12 @@
+﻿namespace BlazorApp2.Models
+{
+    public class employe
+    {
+        public int id { get; set; }
+        public string firstName { get; set; }= string.Empty;
+        public string lastName { get; set; }=string.Empty;
+        public int   age  { get; set; }
+
+
+    }
+}
